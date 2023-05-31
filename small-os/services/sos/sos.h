aece9d0aa07ecd0522ccb774aae6bed1ec9ab816
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2023-05-31 20:59:46 +0300

    Add code document to sos.h

--- small-os/services/sos/sos.h
+++ small-os/services/sos/sos.h
@@ -13,24 +13,30 @@ typedef enum {
 
 /**
  * @brief Tasks configuration structure.
- * 
+ * @var st_SOS_TASK_t::pid
+ * The task's unique ID.
+ * @var st_SOS_TASK_t::process_priority_level
+ * The task's priority level.
+ * This is a value staring from 1, where 1 is the highest priority task.
  */
 typedef struct {
     uint8 pid; uint8 process_priority_level;
 }st_SOS_TASK_t;
 
-static st_SOS_TASK_t *g_ptr_sos_db;
+
+/*Global pointer to the address of the data base.*/
+static st_SOS_TASK_t *g_ptr_sos_db[10];
 
 /**
- * @brief 
- * 
+ * @brief Creates the DB instance and assigns the address of this DB to the global pointer.
+ *
  * @return en_SOS_SYSTEM_STATUS_t 
  */
 en_SOS_SYSTEM_STATUS_t SOS_init(void);
 
 /**
  * @brief De-allocates the created DB instance and points the DB pointer to NULL.
- * 
+ *
  * @return en_SOS_SYSTEM_STATUS_t 
  */
 en_SOS_SYSTEM_STATUS_t SOS_deinit(void);
@@ -63,14 +69,14 @@ void SOS_modifyTask(st_SOS_TASK_t *ptr_st_SOS_DB, uint8 pid, uint8 priority_leve
 
 /**
  * @brief Runs the small OS.
- * 
+ * This is done by loading the timer with a preloaded value which will be the period of every task.
  * @param ptr_st_SOS_DB Address of the tasks database.
  */
 void SOS_run(st_SOS_TASK_t *ptr_st_SOS_DB);
 
 /**
  * @brief Halts the operation of the small OS.
- * 
+ * This is done by disengaging the timer.
  * @param ptr_st_SOS_DB Address of the tasks database.
  */
 void SOS_disable(st_SOS_TASK_t *ptr_st_SOS_DB);
